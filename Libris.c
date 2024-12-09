ca2c2b56acfbc2f583bd78fee658d69b0b92cd8b
kiovaz1 <caiovasconcelos01@live.com>
2024-12-09 16:01:32 -0300

    Deus dai-me forças para continuar

--- Libris.c
+++ Libris.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_LIVROS 100
 #define MAX_USUARIOS 50
 #define MAX_EMPRESTIMOS 100
+
+void clearConsole() {
+    #if defined(_WIN32) || defined(_WIN64)
+        system("cls");  // Limpa o console no Windows
+    #else
+        system("clear");  // Limpa o console no Linux/Mac
+    #endif
+}
 
 // Definições das estruturas
 typedef struct {
@@ -11,7 +20,7 @@ typedef struct {
     char autor[100];
     char anoDePublicacao[10];
     char ISBN[20];
-    char status[20];
+    char status[20]; // "disponivel" ou "emprestado"
 } Livro;
 
 typedef struct {
@@ -46,26 +55,39 @@ int validaEntrada() {
 
 // Funções de gerenciamento de livros
 void listarLivrosDisponiveis() {
+    clearConsole();
     printf("Lista de Livros Disponíveis:\n");
+    int livrosDisponiveis = 0;
     for (int i = 0; i < numLivros; i++) {
         if (strcmp(livros[i].status, "disponivel") == 0) {
-            printf("%d - %s\n", i+1, livros[i].titulo);
+            printf("%d - %s\n", i + 1, livros[i].titulo);
+            livrosDisponiveis++;
         }
+    }
+    if (livrosDisponiveis == 0) {
+        printf("Nenhum livro disponível.\n");
     }
 }
 
 void listarLivrosEmprestados() {
+    clearConsole();
     printf("Lista de Livros Emprestados:\n");
+    int livrosEmprestados = 0;
     for (int i = 0; i < numEmprestimos; i++) {
         int livroID = emprestimos[i].livroID;
-        if (livros[livroID].status[0] == 'e') {
-            printf("%d - %s\n", livroID+1, livros[livroID].titulo);
+        if (strcmp(livros[livroID].status, "emprestado") == 0) {
+            printf("%d - %s (Emprestado por %s)\n", livroID + 1, livros[livroID].titulo, usuarios[emprestimos[i].usuarioID].nome);
+            livrosEmprestados++;
         }
+    }
+    if (livrosEmprestados == 0) {
+        printf("Nenhum livro emprestado.\n");
     }
 }
 
 // Funções de gerenciamento de usuários
 void listarUsuarios() {
+    clearConsole();
     printf("Lista de Usuários:\n");
     for (int i = 0; i < numUsuarios; i++) {
         printf("%d - %s\n", usuarios[i].ID, usuarios[i].nome);
@@ -74,6 +96,7 @@ void listarUsuarios() {
 
 // Função de gerenciamento de empréstimos
 void realizarEmprestimo() {
+    clearConsole();
     int livroID, usuarioID;
     printf("Digite o ID do livro para emprestar: ");
     scanf("%d", &livroID);
@@ -107,15 +130,18 @@ void realizarEmprestimo() {
 void menuLivros() {
     int escolha;
     do {
+        clearConsole();
         printf("\n--- Menu Livros ---\n");
         printf("1. Listar Livros Disponíveis\n");
         printf("2. Listar Livros Emprestados\n");
+        printf("0. Voltar ao menu anterior\n");
         printf("Escolha uma opção: ");
         escolha = validaEntrada();
 
         switch (escolha) {
             case 1: listarLivrosDisponiveis(); break;
             case 2: listarLivrosEmprestados(); break;
+            case 0: break;
             default: printf("Opção inválida!\n"); break;
         }
     } while (escolha != 0);
@@ -124,13 +150,16 @@ void menuLivros() {
 void menuUsuarios() {
     int escolha;
     do {
+        clearConsole();
         printf("\n--- Menu Usuários ---\n");
         printf("1. Listar Usuários\n");
+        printf("0. Voltar ao menu anterior\n");
         printf("Escolha uma opção: ");
         escolha = validaEntrada();
 
         switch (escolha) {
             case 1: listarUsuarios(); break;
+            case 0: break;
             default: printf("Opção inválida!\n"); break;
         }
     } while (escolha != 0);
@@ -139,13 +168,16 @@ void menuUsuarios() {
 void menuEmprestimos() {
     int escolha;
     do {
+        clearConsole();
         printf("\n--- Menu Empréstimos ---\n");
         printf("1. Realizar Empréstimo\n");
+        printf("0. Voltar ao menu anterior\n");
         printf("Escolha uma opção: ");
         escolha = validaEntrada();
 
         switch (escolha) {
             case 1: realizarEmprestimo(); break;
+            case 0: break;
             default: printf("Opção inválida!\n"); break;
         }
     } while (escolha != 0);
@@ -154,23 +186,27 @@ void menuEmprestimos() {
 void menuOutrasFuncoes() {
     int escolha;
     do {
+        clearConsole();
         printf("\n--- Outras Funções ---\n");
         printf("1. Listar Livros Disponíveis\n");
         printf("2. Listar Livros Emprestados\n");
+        printf("0. Voltar ao menu anterior\n");
         printf("Escolha uma opção: ");
         escolha = validaEntrada();
 
         switch (escolha) {
             case 1: listarLivrosDisponiveis(); break;
             case 2: listarLivrosEmprestados(); break;
+            case 0: break;
             default: printf("Opção inválida!\n"); break;
         }
     } while (escolha != 0);
 }
 
+// Função principal
 int main() {
     int escolhaOpcao;
-
+    clearConsole();
     // Exibindo o menu principal
     printf(" ▄█        ▄█  ▀█████████▄     ▄████████  ▄█     ▄████████ \n");
     printf("███       ███    ███    ███   ███    ███ ███    ███    ███ \n");
@@ -186,6 +222,7 @@ int main() {
     printf("2. Gerenciamento de Usuários\n");
     printf("3. Gerenciamento de Empréstimos\n");
     printf("4. Outras Funções\n");
+    printf("0. Sair\n");
     printf("Escolha uma opção: ");
     escolhaOpcao = validaEntrada();
 
@@ -194,7 +231,8 @@ int main() {
         case 2: menuUsuarios(); break;
         case 3: menuEmprestimos(); break;
         case 4: menuOutrasFuncoes(); break;
-        default: printf("Opção inválida! Tente novamente.\n");
+        case 0: exit(0); break;
+        default: printf("Opção inválida! Tente novamente.\n"); break;
     }
 
     return 0;
